7f557a5c897da85110175692a87098d11a662d65
Rudyibz2 <rodolfoscrollini@gmail.com>
2026-07-20 14:38:17 +0200

    Sprint 5 - React frontend initialized

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@
 SmartAnalyticsPlatform
 Punto de entrada principal de la aplicación.
 """
-
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
@@ -84,7 +84,16 @@ async def smartanalytics_exception_handler(
 # =====================================================
 # API
 # =====================================================
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(
     api_router,
     prefix="/api/v1",
